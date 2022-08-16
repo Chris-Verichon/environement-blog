d907c2535d68298840eb6f66aaec232df12c146e
Chris-Verichon <chris.verichon@gmail.com>
2022-08-16 10:00:56 +0200

    integration HTML/CSS Done , create MVC Structure for project and connect to database

---
+++ vendor/composer/installed.php
@@ -0,0 +1,69 @@
+<?php return array (
+  'root' => 
+  array (
+    'pretty_version' => 'dev-master',
+    'version' => 'dev-master',
+    'aliases' => 
+    array (
+    ),
+    'reference' => 'd80d1727b80b0b55c90aac0020b08df5ac56f7ed',
+    'name' => '__root__',
+  ),
+  'versions' => 
+  array (
+    '__root__' => 
+    array (
+      'pretty_version' => 'dev-master',
+      'version' => 'dev-master',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'd80d1727b80b0b55c90aac0020b08df5ac56f7ed',
+    ),
+    'altorouter/altorouter' => 
+    array (
+      'pretty_version' => '2.0.2',
+      'version' => '2.0.2.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'f6fede4f94ced7c22ba63a9b8af0bf2dc38e3cb2',
+    ),
+    'benoclock/alto-dispatcher' => 
+    array (
+      'pretty_version' => '1.3',
+      'version' => '1.3.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'fe7cbb66323a3d5025cc51941b1bdb6cc67be40c',
+    ),
+    'symfony/polyfill-mbstring' => 
+    array (
+      'pretty_version' => 'v1.26.0',
+      'version' => '1.26.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => '9344f9cb97f3b19424af1a21a3b0e75b0a7d8d7e',
+    ),
+    'symfony/polyfill-php80' => 
+    array (
+      'pretty_version' => 'v1.26.0',
+      'version' => '1.26.0.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'cfa0ae98841b9e461207c13ab093d76b0fa7bace',
+    ),
+    'symfony/var-dumper' => 
+    array (
+      'pretty_version' => 'v5.4.11',
+      'version' => '5.4.11.0',
+      'aliases' => 
+      array (
+      ),
+      'reference' => 'b8f306d7b8ef34fb3db3305be97ba8e088fb4861',
+    ),
+  ),
+);
